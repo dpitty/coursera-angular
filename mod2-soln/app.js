a05c3cce61079c36fe37a0bac36c68bef4f53588
David Pitt <dpitt00@gmail.com>
2016-10-25 12:25:50 -0700

    Applied proper decomposition, put business logic in the Service

--- mod2-soln/app.js
+++ mod2-soln/app.js
@@ -7,43 +7,46 @@
 
   HitListController.$inject = ['MurderTrackerService'];
   function HitListController(MurderTrackerService) {
-    var toKill = this;
-    toKill.targetList = [{name: "Trump", bounty: 400}, {name: "Cheney", bounty: 800}, {name: "Rumsfeld", bounty: 80000}];
-    toKill.kill = function (index) {
-      MurderTrackerService.addKill(toKill.targetList[index]);
-      toKill.targetList.splice(index, 1);
+    var targets = this;
+
+    targets.targetList = MurderTrackerService.getTargetList();
+
+    targets.kill = function (index) {
+      MurderTrackerService.kill(index);
     }
-    toKill.addTarget = function() {
-      console.log({name: toKill.newTargetName, bounty: toKill.newTargetBounty});
-      if (isNaN(toKill.newTargetBounty)) {
-        toKill.newTargetBounty = 0;
-      }
-      toKill.targetList.push({name: toKill.newTargetName, bounty: toKill.newTargetBounty});
+
+    targets.addTarget = function() {
+      MurderTrackerService.addTarget(targets.newTargetName, targets.newTargetBounty);
     }
   }
 
   KillBoardController.$inject = ["MurderTrackerService"];
   function KillBoardController(MurderTrackerService) {
-    var killBoard = this;
-    killBoard.victimList = MurderTrackerService.getKilledList();//[{name: "Putin", bounty: 6000}];
-    killBoard.sumBounty = function() {
-      //killBoard.bountyIncome = MurderTrackerService.getBountyIncome();
-      return MurderTrackerService.getBountyIncome();
-    };
+    var kills = this;
+
+    kills.killedList = MurderTrackerService.getKilledList();
+
+    kills.getBountyIncome = function() { return MurderTrackerService.getBountyIncome() };
   }
 
   function MurderTrackerService() {
     var service = this;
-    var hitList = [];
+    var targetList = [{name: "Trump", bounty: 400}, {name: "Cheney", bounty: 800}, {name: "Rumsfeld", bounty: 80000}];
     var killedList = [];
     var bountyIncome = 0;
 
-    service.addKill = function (victim) {
-      //console.log("You killed", victim.name);
-      killedList.push({name: victim.name, bounty: victim.bounty});
-      bountyIncome += Number(victim.bounty);
+
+    service.addTarget = function(name, bounty) {
+      targetList.push({name: name, bounty: bounty});
     }
 
+    service.kill = function (index) {
+      if (!isNaN(targetList[index].bounty)) bountyIncome += targetList[index].bounty;
+      killedList.push(targetList[index]);
+      targetList.splice(index, 1);
+    }
+
+    service.getTargetList = function() { return targetList; }
     service.getKilledList = function() { return killedList; }
     service.getBountyIncome = function() { return bountyIncome; }
   }
